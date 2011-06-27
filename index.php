d2f2cc89e670dbcbb606eff9b4d29ca440e2d43c
Piotr Jasiulewicz <pjasiulewicz@gmail.com>
2011-06-27 14:00:38 +0200

    - added license information - some restructuring in index file

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <?php
 
-
-error_reporting(E_ALL);
-ini_set('display_errors', true);
-
-include_once(realpath(dirname(__FILE__).'/lib/AgileProfiler.php'));
+/**
+ * Running the profiler
+ */
+include_once(realpath(dirname(__FILE__) . '/lib/AgileProfiler.php'));
 
 AgileProfiler::registerAutoload();
-
 AgileProfiler\Profiler\Base::setEventClass('\AgileProfiler\Profiler\Event\Traceable');
 
 $profiler = new AgileProfiler\Profiler\Base();
 
-
-	$profiler->start();
-	usleep(200);
-	$profiler->stop(array('bardziej wewnetrzny task', 'ff0000'));
-
+$profiler->start();
+usleep(200);
+$profiler->stop(array('Some php task', 'ff0000'));
+usleep(300);
+$profiler->start();
+usleep(200);
+$profiler->stop('Some other php task');
 
 $stack = $profiler->getEventStack();
 
 foreach ( $stack as $ev )
 {
 	echo get_class($ev);
-	echo '<p style="color:#' . $ev->getDescription()->getColor() . '">Event: ' . $ev->getDescription();
+	echo '<p style="margin-bottom:50px;color:#' . $ev->getDescription()->getColor() . '">Event: ' . $ev->getDescription();
 	echo '<br />start - ' . $ev->getStartMicrotime();
 	echo '<br />end - ' . $ev->getEndMicrotime();
 	echo '<br />duration - ' . $ev->getTimeDuration();
 	var_dump($ev->getStartTrace());
 	var_dump($ev->getEndTrace());
-	echo '</p>';
+	echo '</p><hr />';
 }
 
